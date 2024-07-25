3d21e1ee1eff31ef714794a190b97790f2000ea6
Michael D. Givens <michael.d.givens@gmail.com>
2024-07-25 18:51:47 -0400

    Added some debug checks before logging to the console.

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -6,7 +6,8 @@ extends Area2D
 
 func _on_body_entered(body):
 	# Notify that the coin has been picked up by the player
-	print("+1 coin!")
+	if OS.is_debug_build():
+		print("+1 coin!")
 	
 	# Remove the coin from the scene when it's collected by the player
 	queue_free()
