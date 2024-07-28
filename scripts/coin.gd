4b8f6e72765d2420befbdd55159447ef36c01a12
kuroiarashi <michael.d.givens@gmail.com>
2024-07-28 13:15:01 -0400

    Added labels for gameplay hints and player score. (#6)

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -3,11 +3,12 @@ extends Area2D
 # Exists on collision layer 1
 # Detects collisions on layer 2 (player)
 
+@onready var game_manager = %GameManager
+
 
 func _on_body_entered(body):
 	# Notify that the coin has been picked up by the player
-	if OS.is_debug_build():
-		print("+1 coin!")
+	game_manager.add_point()
 	
 	# Remove the coin from the scene when it's collected by the player
 	queue_free()
--- scripts/slime.gd
+++ scripts/slime.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const SPEED = 60
+const SPEED : int = 60
 
 var direction = 1
 
