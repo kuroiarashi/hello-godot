faa6af874aa704e14e8b1973ef2a9e30be8bfcc1
kuroiarashi <michael.d.givens@gmail.com>
2024-07-28 11:35:05 -0400

    Improvements for player character (#5)
    * Improvements for player death animation.
    
    * Improvements for player movement controls and animations.

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -7,9 +7,18 @@ func _on_body_entered(body):
 	if OS.is_debug_build():
 		print("You died!")
 	
+	# Slow time
+	Engine.time_scale = 0.5
+	
+	# Remove the player collider to let the 
+	# player fall off the map
+	body.get_node("CollisionShape2D").queue_free()
+	
+	# Start the respawn timer
 	timer.start()
 
 
 func _on_timer_timeout():
 	# Restarts the scene after death
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
